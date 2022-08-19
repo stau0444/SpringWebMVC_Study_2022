dd17ffce75c9a068119f73e72836ae7a35be0967
stau04@gmail.com <stau04@gmail.com>
2022-08-19 22:32:50 +0900

    first commit

---
+++ src/main/java/com/study/springmvc_2022/SpringMvc2022Application.java
@@ -0,0 +1,13 @@
+package com.study.springmvc_2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvc2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvc2022Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/springmvc_2022/SpringMvc2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springmvc_2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
